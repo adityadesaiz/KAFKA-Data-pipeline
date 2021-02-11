26666bd0ed0d1a60798e9ec22f276961bd77f33a
Aditya S. Desai <20984638+adityadesaiz@users.noreply.github.com>
2021-02-11 12:55:10 +0530

    Create kafka-data-producer.py

---
+++ kafka-data-producer.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+from confluent_Kafka import Producer
+
+if __name__ == '__main__':
+    topic = os.environ['Kafka_TOPIC'].split(",")[0]
+
+   
+    conf = {
+        'bootstrap.servers': os.environ['Kafka_BROKERS'],
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+	'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': os.environ['Kafka_USERNAME'],
+        'sasl.password': os.environ['Kafka_PASSWORD']
+    }
+
+    p = Producer(**conf)
+
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d]\n' %
+                             (msg.topic(), msg.partition()))
+
+    for line in sys.stdin:
+        try:
+            p.produce(topic, line.rstrip(), callback=delivery_callback)
+        except BufferError as e:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+        p.poll(0)
+
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
